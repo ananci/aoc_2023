ddb1224e25fab1ef445244fcf63cae5c02eb2166
Anna Eilering <nahkki@gmail.com>
2023-12-01 19:10:04 +0000

    Code Golfed day 1.

--- day1/1_2.py
+++ day1/1_2.py
@@ -2,46 +2,31 @@ import re
 val_dict = {'one': '1', 'two':'2', 'three':'3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
 total = 0
 
-def get_ints(line:str) -> list[int]:
-    regex_group_str = '|'.join(val_dict.keys())
-    regex_group_str = f'(?=({regex_group_str}|\d))'
-    found = re.findall(regex_group_str, line)
-    return_list = []
-    for f in found:
-        val = val_dict.get(f, f)
-        return_list.append(int(val))
-    return return_list
-
-def get_first_last_new_number(ints: list[int]) -> int:
-    if len(ints)<2:
-        val = ints[0]*11
-    else:
-        val = ints[0]*10 + ints[-1]
-    return val
-
-def get_sum_from_list(lines:list[str]) -> int:
+def get_sum(lines: list[str]) -> int:
     total = 0
+    val_list = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    regex_group_str = f"(?=({'|'.join(val_list)}|\d))"
     for line in lines:
-        ints = get_ints(line)
-        val = get_first_last_new_number(ints)
-        total += val
+        found = [x if x not in val_list else str(val_list.index(x) + 1) for x in re.findall(regex_group_str, line)]
+        val = found[0] + found[-1]
+        total += int(val)
     return total
 
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        lines = f.readlines()
+        print(f'From input: {get_sum(lines)}')
 
-with open('input.txt', 'r') as f:
-    lines = f.readlines()
-    print(f'From input: {get_sum_from_list(lines)}')
+    test_list = [
+        # 'two1nine',
+        # 'eightwothree',
+        # 'abcone2threexyz',
+        # 'xtwone3four',
+        # '4nineeightseven2',
+        # 'zoneight234',
+        # '7pqrstsixteen',
+        'eighthree',
+        'sevenine'
+    ]
 
-
-test_list = [
-    # 'two1nine',
-    # 'eightwothree',
-    # 'abcone2threexyz',
-    # 'xtwone3four',
-    # '4nineeightseven2',
-    # 'zoneight234',
-    # '7pqrstsixteen',
-    'eighthree',
-    'sevenine'
-]
-print(f'From test input: {get_sum_from_list(test_list)}')
+    print(f'From test input: {get_sum(test_list)}')
