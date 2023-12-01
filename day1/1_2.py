cb979f4720896f53c7e75858cb985f9eb6c1750c
Anna Eilering <nahkki@gmail.com>
2023-12-01 19:10:38 +0000

    Adding new code golfed day 1 impls.

--- day1/1_2.py
+++ day1/1_2.py
@@ -1,6 +1,4 @@
 import re
-val_dict = {'one': '1', 'two':'2', 'three':'3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
-total = 0
 
 def get_sum(lines: list[str]) -> int:
     total = 0
